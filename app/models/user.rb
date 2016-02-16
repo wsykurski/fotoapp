bcd689cf7b93ceecc14e8d9741edcc07e9c391ae
Wojciech Sykurski <w.sykurski@samsung.com>
2016-02-16 11:01:06 +0100

    Photo added to user-show

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ActiveRecord::Base
                     :url => "/system/:attachment/:id/:basename_:style.:extension",
                     :styles => {
                       :original => [:format => :jpg, :quality => 70],
-                      :mysingle => ['300x400', :jpg, :quality => 70] 
+                      :mysingle => ['200x300', :jpg, :quality => 70] 
                     },
                     :convert_options => {
                       :original => '-set colorspacesRGB -strip',
                       :mysingle => '-set colorspacesRGB -strip'
                     }
+  validates_attachment :photo,
+                       :presence => true,
+                       :size => { :in => 0..10.megabytes },
+                       :content_type => { :content_type => /^image\/(jpeg|png|gif|tiff)$/}
 end
