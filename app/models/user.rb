54e29e5c4d8e4d8496b1258e962f390983e5d42d
Wojciech Sykurski <w.sykurski@samsung.com>
2016-02-16 10:45:35 +0100

    Paperclip integrated into User
    - Model
    - Edit Form
    - Show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  has_attached_file :photo,
+                    :path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
+                    :url => "/system/:attachment/:id/:basename_:style.:extension",
+                    :styles => {
+                      :original => [:format => :jpg, :quality => 70],
+                      :mysingle => ['300x400', :jpg, :quality => 70] 
+                    },
+                    :convert_options => {
+                      :original => '-set colorspacesRGB -strip',
+                      :mysingle => '-set colorspacesRGB -strip'
+                    }
 end
